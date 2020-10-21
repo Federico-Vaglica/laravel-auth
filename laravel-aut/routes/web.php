b18112a7b14e1c9f97673c9ae68aafabea2c4bca
federicov <vaglicafederico95@gmail.com>
2020-10-21 17:54:51 +0200

    Create Done

--- laravel-aut/routes/web.php
+++ laravel-aut/routes/web.php
@@ -21,10 +21,7 @@ Auth::routes();
 
 // Route::get('admin/home', 'Admin\HomeController@index')->name('home');
 
-Route::prefix('admin')
-->namespace('Admin')
-->middleware('auth')
-->group(function () {
-Route::get('/', 'HomeController@index')
-->name('home');
+Route::prefix('admin')->namespace('Admin')->middleware('auth')->group(function () {
+    Route::get('/home', 'HomeController@index')->name('home');
+    Route::resource('posts', 'PostController');
 });
